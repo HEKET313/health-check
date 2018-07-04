98becd9525afbb527cb32e5dd0131fcd34ca8df5
Nikita Leshchev <neket313@gmail.com>
2018-07-04 18:12:02 +0400

    BAC-1151: Add one more status

--- src/Entity/HealthDataInterface.php
+++ src/Entity/HealthDataInterface.php
@@ -5,8 +5,9 @@ namespace Optimax\HealthCheckBundle\Entity;
 interface HealthDataInterface
 {
     public const STATUS_OK = 1;
-    public const STATUS_ERROR = 2;
-    public const STATUS_WARNING = 3;
+    public const STATUS_WARNING = 2;
+    public const STATUS_DANGER = 3;
+    public const STATUS_CRITICAL = 4;
 
     public function getStatus(): int;
     public function getAdditionalInfo(): array;
