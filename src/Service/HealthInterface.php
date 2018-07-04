26483e482ae612ebc076602667ff06b7d3d67881
Nikita Leshchev <neket313@gmail.com>
2018-07-04 16:58:59 +0400

    BAC-1151: Use constant as tag

--- src/Service/HealthInterface.php
+++ src/Service/HealthInterface.php
@@ -6,6 +6,8 @@ use Optimax\HealthCheckBundle\Entity\HealthDataInterface;
 
 interface HealthInterface
 {
+    public const TAG = 'health.service';
+
     public function getName(): string;
     public function getHealthInfo(): HealthDataInterface;
 }
